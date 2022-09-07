6c5684e1f63b4cddd88739628b2c1033e206caec
GeeksMinister <nael_sps@hotmail.com>
2022-09-07 14:51:50 +0200

    Built the addEmployy component. Renamed project. Might need to create new one for fixing a bug with the debbuger

--- DotNetAndAngular_UI/karma.conf.js
+++ DotNetAndAngular_UI/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dot-net-and-angular.ui'),
+      dir: require('path').join(__dirname, './coverage/dot-net-and-angular_ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
